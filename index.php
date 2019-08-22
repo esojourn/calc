eb931792eff094d2f1a7eded00f679649f6fd030
esojourn <esojourn@gmail.com>
2019-08-22 11:27:43 +0800

    格式调整。

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $kmh_str = '';
 $error = FALSE;
 $msg = '';
 $who = '';
-$table_counter=0;
+$table_counter = 0;
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
   $pace = htmlspecialchars($_POST["pace"]);
@@ -27,26 +27,27 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     }
   } elseif ($who == 'pace') { //
     if (is_numeric($pace)) {
-      if(intval($pace)<=200||intval($pace)>=999){
+      if (intval($pace) <= 200 || intval($pace) >= 999) {
         $error = TRUE;
         $msg .= "别逗我，配速不合常识~<br>\n";
       }
 
     } else {
       $error = TRUE;
-      $msg .= "配速你填".$pace."，打算让我怎么算？<br>\n";
+      $msg .= "配速你填" . $pace . "，打算让我怎么算？<br>\n";
     }
   }
   work();
 }
 
-function convertToHoursMins($time, $format = '%02d:%02d:%02d') {
+function convertToHoursMins($time, $format = '%02d:%02d:%02d')
+{
   if ($time < 1) {
     return;
   }
   $hours = floor($time / 3600);
-  $minutes = floor(($time -$hours*3600) / 60);
-  $seconds = (($time -$hours*3600-$minutes*60)  % 3600);
+  $minutes = floor(($time - $hours * 3600) / 60);
+  $seconds = (($time - $hours * 3600 - $minutes * 60) % 3600);
   return sprintf($format, $hours, $minutes, $seconds);
 }
 
@@ -60,7 +61,7 @@ function work()
   if ($who == 'pace') {
     $sec = (int)substr($pace, -2);
     $min = (int)substr($pace, 0, strlen($pace) - 2);
-    $kmh_str = strval(round(1 / ($min * 60 + $sec) * 3600,2));
+    $kmh_str = strval(round(1 / ($min * 60 + $sec) * 3600, 2));
 
 
   } elseif ($who == 'kmh') {
@@ -99,33 +100,37 @@ function work()
   <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/grids-responsive-min.css">
   <!--<![endif]-->
   <style type="text/css">
-    h1,h2 {
+    h1, h2 {
       font-family: "Nexa", "微软雅黑", "Open Sans", "Noto Sans S Chinese", sans-serif;
       font-size: 20px;
     }
-    .error{color:#f00;}
 
-    .content  {
+    .error {
+      color: #f00;
+    }
+
+    .content {
       text-align: center;
       vertical-align: middle;
       margin: 0 10px;
     }
 
-    .content-inner{
+    .content-inner {
       margin: 0 auto;
     }
-    .table{
+
+    .table {
       text-align: center;
       vertical-align: middle;
     }
 
-    .table table{
+    .table table {
       min-width: 300px;
 
       margin: 0 auto;
     }
 
-      .who-active {
+    .who-active {
       /*border-color: #0078e7;*/
       background-color: #d8c74a;
       color: #fff;
@@ -137,10 +142,11 @@ function work()
     }
 
     @media (max-width: 767px) {
-      .pure-form input[type=text]{
+      .pure-form input[type=text] {
         display: inline-block;
       }
-      .pure-form-aligned .pure-control-group label{
+
+      .pure-form-aligned .pure-control-group label {
         text-align: center;
         display: inline-block;
       }
@@ -228,9 +234,9 @@ function work()
       </fieldset>
     </form>
     <?php
-    if(!$error&& $_SERVER["REQUEST_METHOD"] == 'POST'){
+    if (!$error && $_SERVER["REQUEST_METHOD"] == 'POST') {
 
-      $distance=array(
+      $distance = array(
         '0.1' => '100m',
         '0.2' => '200m',
         '0.4' => '400m',
@@ -241,7 +247,7 @@ function work()
         '21.0975' => '半马',
         '42.195' => '全马',
       );
-      $time=array(
+      $time = array(
         1,
         2,
         5,
@@ -251,7 +257,7 @@ function work()
         30,
         60,
         120,
-        );
+      );
 
       echo '<h2>在此速度下</h2>';
       echo "<div class=\"pure-g\"><div class='table pure-u-1'>";
@@ -265,18 +271,18 @@ function work()
       ";
 
 
-      foreach ($distance as $key => $value){
-        $table_counter+=1;
-        if($table_counter%2){
+      foreach ($distance as $key => $value) {
+        $table_counter += 1;
+        if ($table_counter % 2) {
           //odd
           echo "<tr class=\"pure-table-odd\">";
-        }else{
+        } else {
           //even
           echo "<tr>";
         }
-        echo '<td>'.$value.'</td>';
-        $time_ori=(float)(1/floatval($kmh_str)*(floatval($key))*3600);
-        echo '<td>'.convertToHoursMins($time_ori).'</td></tr>';
+        echo '<td>' . $value . '</td>';
+        $time_ori = (float)(1 / floatval($kmh_str) * (floatval($key)) * 3600);
+        echo '<td>' . convertToHoursMins($time_ori) . '</td></tr>';
       }
       echo "</tbody></table>";
 
@@ -284,7 +290,7 @@ function work()
       echo "</div>";
 
 
-     echo "<div class='table pure-u-1'>";
+      echo "<div class='table pure-u-1'>";
       echo "<table class=\"pure-table\">
     <thead>
         <tr>
@@ -293,19 +299,19 @@ function work()
         </tr>
     </thead><tbody>
       ";
-      $table_counter=0;
-      foreach ($time as $key => $value){
-        $table_counter+=1;
-        if($table_counter%2){
+      $table_counter = 0;
+      foreach ($time as $key => $value) {
+        $table_counter += 1;
+        if ($table_counter % 2) {
           //odd
           echo "<tr class=\"pure-table-odd\">";
-        }else{
+        } else {
           //even
           echo "<tr>";
         }
-        echo '<td>'.$value.'分</td>';
-        $time_ori=round($value/60*floatval($kmh_str)*1000,0);
-        echo '<td>'. $time_ori . '米</td></tr>';
+        echo '<td>' . $value . '分</td>';
+        $time_ori = round($value / 60 * floatval($kmh_str) * 1000, 0);
+        echo '<td>' . $time_ori . '米</td></tr>';
       }
       echo "</tbody></table>";
 
@@ -319,21 +325,24 @@ function work()
 
       echo "</div>";
 
-    }else{
+    } else {
 
     }
     echo '网上搜索到的配速转换器都觉得不好用。<br>
-      自己写了一个。欢迎跑友测试。<br><br>bug反馈：<br>Q: 67903166, M: esojourn@qq.com<br>
-<a href="https://dingxuan.info">dingxuan.info</a></div>';
+      自己写了一个。欢迎跑友测试。<br><br>
+      <a href="https://github.com/esojourn/calc/issues">bug反馈</a> - 
+      <a href="https://github.com/esojourn/calc">源代码</a> - 
+      <a href="https://dingxuan.info/calc">跑步配速转换</a><br>
+      <a href="https://dingxuan.info">dingxuan.info</a></div>';
     ?>
     <div class="table">
 
 
       <?php
       //echo $msg; //21.0975, 42.195
-/*      echo 'Pace value: ' . $pace_str . '<br>';
-      echo 'KMH value: ' . $kmh_str . '<br>';
-      echo 'WHO: ' . $who . '<br>';*/
+      /*      echo 'Pace value: ' . $pace_str . '<br>';
+            echo 'KMH value: ' . $kmh_str . '<br>';
+            echo 'WHO: ' . $who . '<br>';*/
 
       ?>
     </div>
@@ -345,7 +354,11 @@ function work()
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-248898-1"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
     gtag('js', new Date());
 
     gtag('config', 'UA-248898-1');
